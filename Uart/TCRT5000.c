8a6d56b40919070508e91619962a899555490ea0
887YUHG <3565975130@qq.com>
2026-08-19 21:19:18 +0800

    Update TCRT5000.c

--- Uart/TCRT5000.c
+++ Uart/TCRT5000.c
@@ -5,12 +5,12 @@
 extern volatile uint8_t car_stop_flag;
 extern uint8_t task;
 /******************************************************************
- * �� �� �� �ƣ�Get_OUT1
- * �� �� ˵ ������ȡ������ʶ��״̬
- * �� �� �� �Σ���
- * �� �� �� �أ�1=ʶ��Ϊ��ɫ   0=ʶ��Ĳ��Ǻ�ɫ
- * ��       �ߣ�LCKFB
- * ��       ע������ͨ��ģ���ϵĿɵ��������ʶ���ɫ����ֵ
+ * 函 数 名 称：Get_OUT1
+ * 函 数 说 明：读取传感器识别状态
+ * 函 数 形 参：无
+ * 函 数 返 回：1=识别为黑色   0=识别的不是黑色
+ * 作       者：LCKFB
+ * 备       注：可以通过模块上的可调电阻调整识别黑色的阈值
 ******************************************************************/
 unsigned char Get_OUT1(void)
 {
@@ -111,7 +111,7 @@ unsigned char Get_OUT8(void)
 }
 
 
-//// ��ȡ8·������=0����ֽ=1����һ�ζ�ȡ�˸�״̬��bit7=S1 ... bit0=S8
+//读取8 路循迹红外传感器 S1~S8，把 8 个传感器的高低电平，打包到1 个 uint8_t (8 位字节) 整数返回
 uint8_t Read_8Track_Sensor(void)
 {
     uint8_t val = 0;
@@ -456,18 +456,7 @@ void Track_8Sensor_Process_AA(uint8_t track_buf, float speed_run)
             target_speed_left = speed_run - 160;
             target_speed_right = speed_run + 160;
             break;            		
-/*		
-        case 224:   // S1+S2+S3检测到 (1110 0000)			
-        case 240:   // 左半边全检测到 (1111 0000)
-            target_speed_left = speed_run - 85;   // 大幅减速/反转
-            target_speed_right = speed_run + 70;   // 加速
-            break;           
-        case 112:   // S2+S3+S4检测到 (0111 0000)
-        case 120:   // S2+S3+S4+S5检测到 (0111 1000)
-            target_speed_left = speed_run - 60;
-            target_speed_right = speed_run + 50;
-            break;
-*/		
+	
         // --- 严重向右偏 ---
         case 1:     // 仅S8检测到 (0000 0001) - 最右边！
 		case 3:		// 仅S7 S8,检测到 (0000 0011) - 最右边！
@@ -504,101 +493,3 @@ void Track_8Sensor_Process_AA(uint8_t track_buf, float speed_run)
     }
 }
 
-/*
-//// 读取5路（黑线=0，白纸=1）    这个是封装好的，可以一次读取五个状态
-//uint8_t Read_5Track_Sensor(void)
-//{
-//    uint8_t val = 0;
-//    val |= (S1) << 4;
-//    val |= (S2) << 3;
-//    val |= (S3) << 2;
-//    val |= (S4) << 1;
-//    val |= (S5) << 0;
-//    return val;
-//}
-
-//void Track_8Sensor_Process(uint8_t track_buf, float speed_run)
-//{
-//	switch(track_buf)
-//         {
-//				// 居中直行（最稳）
-//				case 0:	
-//					// Car_Forward(CAR_SPEED+7,CAR_SPEED);
-//				target_speed_left = speed_run;
-//                target_speed_right = speed_run;
-//				//DL_GPIO_setPins(GPIO_PORT, GPIO_IN1_PIN);	
-//				  break;
-//				case 4:
-//				//	case 31:
-//				   target_speed_left = speed_run - 40;
-//                   target_speed_right = speed_run - 40;
-//				  // Car_Forward(CAR_SPEED+7,CAR_SPEED);
-//				  // delay_ms(10);
-//					break;
-//				// 需要轻微向左转向，来矫正姿态（S4）
-//				case 12:
-//				  // Car_Forward(CAR_SPEED, OFFSET_MID);   // 200    170
-//					target_speed_left = speed_run - 23;
-//					target_speed_right = speed_run +20;
-//				 //  delay_ms(50);
-//					break;		
-//				// 需要中度向左转向，来矫正姿态（out2）
-//				case 24:
-//				case 8:
-//				  // Car_Forward(OFFSET_MID - OFFSET_SMALL, OFFSET_MID);	
-//					target_speed_left = speed_run - 65;
-//					target_speed_right = speed_run +20;
-//					//delay_ms(100);
-//					break;		
-//				// 需要大幅度向左转向，来矫正姿态（S5） BIG_TURN       
-//				case 16:
-//					//Car_Forward(CAR2_SPEED - BIG_TURN, CAR2_SPEED ); 
-//					target_speed_left = speed_run - 85;
-//					target_speed_right = speed_run + 60;
-//					//delay_ms(160);	
-//					break;
-//				
-//				 // 需要轻微向右转向，来矫正姿态（S2）
-//				case 6:
-//					 //Car_Forward( OFFSET_MID+10, CAR_SPEED); 
-//					target_speed_left = speed_run +20;
-//					target_speed_right = speed_run - 55;
-//				
-//				  // delay_ms(50);		
-//					break;		
-//				// 需要中度向右转向，来矫正姿态（S2+S3）
-//				case 3:
-//				case 2:
-//					//Car_Forward(OFFSET_MID+4 , OFFSET_MID - OFFSET_SMALL);  
-//					target_speed_left =speed_run +20;
-//					target_speed_right = speed_run - 65;
-//					//delay_ms(70);	
-//					break;     
-//				// 需要大幅度向右转向，来矫正姿态（S1+S2）BIG_TURN
-//				case 1:    
-//					//Car_Forward(CAR2_SPEED-26, CAR2_SPEED - BIG_TURN-5);
-//					target_speed_left = speed_run + 70;
-//					target_speed_right = speed_run - 85;				
-//					//delay_ms(160);
-//					break;
-//				
-//				// 全黑=终点/十字停车
-//				case 31:
-//				case 7:
-//				case 14:	
-//				case 28:	
-//					//TB6612_Motor_Stop();
-//					target_speed_left = 0;
-//					target_speed_right = 0;
-//					car_stop_flag = 1;
-//					break;
-//				default:
-//					//Car_Forward(CAR_SPEED, CAR_SPEED);
-//					break;
-//            }
-
-
-
-//}
-
-*/
